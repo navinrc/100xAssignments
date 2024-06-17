f500061423b97c2f315045a0a284c32a07fca43b
navinrc <navinrc98@gmail.com>
2024-06-18 00:16:53 +0530

    solved todo hard

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,40 @@
 */
 
 class Todo {
-
+  constructor(todoList = []) {
+    this.todos = todoList;
+  }
+  add(todo) {
+    this.todos.push(todo);
+    return this.todos;
+  }
+  remove(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      this.todos.splice(indexOfTodo, 1);
+      return this.todos;
+    }
+  }
+  update(index, updatedTodo) {
+    if (index >= 0 && index < this.todos.length) {
+      this.todos[index] = updatedTodo;
+    }
+    return this.todos;
+  }
+  getAll() {
+    return this.todos;
+  }
+  get(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      return this.todos[indexOfTodo];
+  } else {
+      return null;
+  }
+  }
+  clear() {
+    return (this.todos = []);
+  }
 }
+
+console.log(new Todo(["Task 1", "Task 2"]).remove(0));
 
 module.exports = Todo;
