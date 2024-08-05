46b6a1a1d28168d6409c7966e83adfb89495469b
navinrc <navinrc98@gmail.com>
2024-08-05 10:24:04 +0530

    completed endpoints in basec auth mongo 3

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,59 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
+const { Admin, Course } = require("../db/index");
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  // Implement admin signup logic
+  Admin.create({
+    username,
+    password,
+  })
+    .then((response) => {
+      if (response)
+        res.status(200).json({
+          message: "Admin created successfully",
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Something is wrong",
+      });
+    });
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post("/courses", adminMiddleware, async (req, res) => {
+  try {
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const price = req.body.price;
+    const imageLink = req.body.imageLink;
+    const courseId = await Course.create({
+      title,
+      description,
+      price,
+      imageLink,
+    });
+    res.json({
+      message: "Course created successfully",
+      courseId: courseId._id,
+    });
+  } catch (error) {
+    console.error("Something went wrong creating course for Admin", error);
+  }
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, async (req, res) => {
+  // Implement fetching all courses logic
+  const courses = await Course.find({});
+  res.json({
+    courses,
+  });
 });
 
 module.exports = router;
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,22 +1,83 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
-
+const { User, Course } = require("../db/index");
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", (req, res) => {
+  console.log("signup user");
+  // Implement user signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+  // Implement admin signup logic
+  User.create({
+    username,
+    password,
+  })
+    .then((response) => {
+      if (response)
+        res.status(200).json({
+          message: "User created successfully",
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Something is wrong",
+      });
+    });
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get("/courses", async (req, res) => {
+  // Implement listing all courses logic
+  const courses = await Course.find({});
+  res.json({
+    courses,
+  });
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
+  // Implement course purchase logic
+  try {
+    const courseId = req.params.courseId;
+    const username = req.headers.username;
+
+    await User.updateOne(
+      {
+        username,
+      },
+      {
+        $push: {
+          purchasedCourses: courseId,
+        },
+      }
+    );
+    res.json({
+      message: "Course purchased successfully",
+    });
+  } catch (error) {
+    console.error("error purchasing course for the user", error);
+  }
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get("/purchasedCourses", userMiddleware, async (req, res) => {
+  // Implement fetching purchased courses logic
+  try {
+    const user = await User.findOne({
+      username: req.headers.username,
+    });
+
+    const courses = await Course.find({
+      _id: {
+        $in: user.purchasedCourses,
+      },
+    });
+
+    res.json({
+      courses: courses,
+    });
+  } catch (error) {
+    console.error("error purchasing course for the user", error);
+  }
 });
 
-module.exports = router
+module.exports = router;
